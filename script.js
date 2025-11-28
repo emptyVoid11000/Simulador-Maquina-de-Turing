8b188499ff2b11b995ff457e28ade45ecc0945b4
Aleja <nd144592@gmail.com>
2025-11-28 15:16:08 -0500

    maquina-de-turing vs2

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ function capturar() {
 }
 
 function crearCinta(){
-    var left = 30;
+    var left = 40;
     const cuadro = document.getElementById("cuadro-lista");
     cuadro.children[0].textContent=cinta[0];
     for (let index = 1; index < cinta.length; index++) {
-        left+=110;
+        left+=120;
         const copia = cuadro.cloneNode(true);
-        copia.children[0].textContent=cinta[index];
+        cinta[index]!=" "? copia.children[0].textContent=cinta[index]: copia.children[0].textContent="/0";
         copia.style.left = left.toString() + "px";
         cuadro.parentNode.appendChild(copia);
     }
@@ -34,18 +34,26 @@ function validar(){
     var caracter = cinta[currentStep];
     if(/^[A-Za-z0-9]$/.test(caracter) || (currentStep>7 && caracter==" ")){
         registroDeEstado="Valido";
-        document.getElementById("output").style.backgroundColor="lightgreen";
-        document.getElementById("validez").textContent = "Válido"
+        document.getElementById("output-text").style.backgroundColor="lightgreen";
+        var text = document.getElementById("validez");
+        text.textContent = registroDeEstado;
+        text.className = "text-success";
+        if((currentStep>7 && caracter==" ")){
+            terminado=true;
+        }
         return true;
     }
     else {
         registroDeEstado="Invalido";
-        document.getElementById("output").style.backgroundColor="lightcoral";
-        document.getElementById("validez").textContent = "Inválido"
+        document.getElementById("output-text").style.backgroundColor="lightcoral";
+        var text = document.getElementById("validez");
+        text.textContent =registroDeEstado;
+        text.className = "text-danger";
         if(!(/^[A-Za-z0-9]$/.test(caracter))){
             terminado=true;
             return false;
         } return false;
+        
         
     }
 }
@@ -58,25 +66,29 @@ function escribir(){
 function currentState(){
     var state =document.getElementById("state");
     if(currentStep>7){state.textContent = "q8"; return;}
-    if(cadena[currentStep]!=" "){state.textContent = "q"+(currentStep)};
-    
+    else {state.textContent = "q"+(currentStep)};
 }
 
 function moverCabezal(){
     var cabezal = document.getElementById("cabezal");
-    let leftStr = window.getComputedStyle(cabezal).left;
+    var flechaArriba = document.getElementById("flecha-arriba");
+    let leftStr1 = window.getComputedStyle(cabezal).left;
+    let leftStr2 = window.getComputedStyle(flechaArriba).left;
 
-    let left = parseInt(leftStr);
-    cabezal.style.left=left+115 + "px";
+    let left1 = parseInt(leftStr1);
+    let left2= parseInt(leftStr2);
+    cabezal.style.left=left1+120 + "px";
+    flechaArriba.style.left=left2+120 + "px";
 }
 
 function validarCaracterEnLaMaquina(){
         validar();
         currentState();
-        document.getElementById("step").textContent="Step "+(currentStep);
+        document.getElementById("step").textContent="Step "+(currentStep+1);
         if(!terminado){
-            escribir();
+            
             moverCabezal();
+            escribir();
         }     
 }
 
@@ -115,7 +127,7 @@ function reiniciar(){
 
     document.getElementById("cadena").value = "";
 
-    document.getElementById("step").textContent="";
+    document.getElementById("step").textContent="Step 0";
 
     var output = document.getElementById("output-text");
 
@@ -131,8 +143,12 @@ function reiniciar(){
         if(i>0) c.remove();
         else c.children[0].textContent=" ";
      })
+    const cuadro = document.getElementById("cuadro-lista");
+    cuadro.children[0].textContent="/0";
     const cabezal = document.getElementById("cabezal");
-    cabezal.style.left="20px";
+    cabezal.style.left="40px";
+    const flecha = document.getElementById("flecha-arriba");
+    flecha.style.left="80px";
 }
 
 
